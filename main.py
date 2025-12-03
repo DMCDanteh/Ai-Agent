a7c2b3611099024e0212e81d94e36d8b7c392908
DMCDanteh <robert_buntic@hotmail.com>
2025-12-03 10:00:51 +0100

    Added user prompts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
 
@@ -8,7 +9,12 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
-    response = client.models.generate_content(model="gemini-2.0-flash-001", contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    response = client.models.generate_content(model="gemini-2.5-flash", contents=args.prompt)
+    if response.usage_metadata == None:
+        raise RuntimeError("Failed API request")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
